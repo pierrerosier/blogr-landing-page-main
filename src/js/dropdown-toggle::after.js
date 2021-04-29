4e74c1588d4cf702776778933a1cf506e0de8e69
pierrerosier <contact@pierrerosier.fr>
2021-04-29 12:08:36 +0200

    Màj du 29/04/21

---
+++ src/js/dropdown-toggle::after.js
@@ -0,0 +1,30 @@
+
+//-----MODIFICATION DE L'APPARENCE DES LIENS CLIQUÉS DU MENU----//
+
+const $navbarDropdown = document.querySelectorAll('#navbarDropdown')
+let navItem = null;
+let presentAdress = "";
+presentAdress = window.location;
+
+//$navbarDropdown va chercher plusieurs éléments, il faut donc les parcourir
+for (let i=0 ; i < $navbarDropdown.length; i++) {
+
+    //ajout du gestionnaire d'évènement 'click'
+    $navbarDropdown[i].addEventListener('click', (evt) => {
+
+        // si navItem n'est pas l'élément auquel le gestionnaire d'evt est attaché,
+        //cela signifie qu'il n'a pas la class 'active', il faut donc lui supprimer si il était précédement 'active'.
+        if (navItem !== evt.currentTarget) {
+            evt.currentTarget.classList.toggle('active');
+            //document.getElementById('navbarDropdown').classList.add('active')
+        }
+        //si navItem n'est pas null (il a donc déjà été visité mais n'est plus visé) et
+        // qu'il n'est pas l'élément auquel le gestionnaire d'evt est attaché,
+        //il faut supprimer la class 'active'.
+        if (navItem !== null && navItem !== evt.currentTarget) {
+            navItem.classList.remove('active');
+            //document.getElementById('navbarDropdown').classList.add('active')
+        }
+        navItem = evt.currentTarget;
+    }, false);
+}
